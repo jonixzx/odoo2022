04888c4e439091d52a3b8b3909dfeed8fbb5ab6d
Johan Verdoodt <jverdoodt@gmail.com>
2022-10-10 10:53:06 +0200

    TEst johan eerste commit

---
+++ estate/models.py
@@ -0,0 +1,24 @@
+from psycopg2 import Date
+from odoo import fields, models
+
+class TestModel(models.Model):
+    _name = "test.model"
+    _description = "Test Model"
+
+    name = fields.Char()
+    description = fields.Text()
+    postcode = fields.Char()    
+    date_availability =fields.Date()
+    expected_price = fields.Float()
+    selling_price = fields.Float()
+    bedrooms = fields.Integer()
+    living_area = fields.Integer()
+    facades = fields.Integer()
+    garage = fields.Boolean()
+    garden = fields.Boolean()
+    garden_area = fields.Integer()
+    garden_orientation = fields.Selection(
+        string='Type',
+        selection=[('north', 'North'), (south', 'South'),('east', 'East'), ('west', 'West')],
+        help="Kies aub")
+    active = fields.Boolean('Active', readonly=True)
